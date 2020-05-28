da9736e61d7e7541817833c0c6d760f91c94e6e9
Brian Clapner <brian.clapner@coxautoinc.com>
2020-05-28 14:55:10 -0400

    validating access token

--- functions/saveHighScore.js
+++ functions/saveHighScore.js
@@ -1,14 +1,20 @@
 const { table, getHighScores } = require('./utils/airtable');
-const { getAccessTokenFromHeaders } = require('./utils/auth');
+const {
+	getAccessTokenFromHeaders,
+	validateAccessToken,
+} = require('./utils/auth');
 
 exports.handler = async (event) => {
 	const token = getAccessTokenFromHeaders(event.headers);
-	if (!token) {
+	let user = await validateAccessToken(token);
+
+	if (!user) {
 		return {
 			statusCode: 401,
-			body: JSON.stringify({ err: 'User is not logged in' }),
+			body: JSON.stringify({ err: 'User is not authorized' }),
 		};
 	}
+
 	if (event.httpMethod !== 'POST') {
 		return {
 			statusCode: 405,
@@ -16,7 +22,9 @@ exports.handler = async (event) => {
 		};
 	}
 
-	const { score, name } = JSON.parse(event.body);
+	const name = user['https://lrnbldtyp/username'];
+	const { score } = JSON.parse(event.body);
+
 	if (typeof score === 'undefined' || !name) {
 		return {
 			statusCode: 400,
--- functions/utils/auth.js
+++ functions/utils/auth.js
@@ -1,3 +1,37 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const jwks = require('jwks-rsa');
+const { promisify } = require('util');
+
+const jwksClient = jwks({
+	jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+});
+
+let signingKey;
+
+const validateAccessToken = async (token) => {
+	if (!signingKey) {
+		try {
+			const getSigningKey = promisify(jwksClient.getSigningKey);
+			const key = await getSigningKey(process.env.AUTH0_KEY_ID);
+			signingKey = key.getPublicKey();
+		} catch (error) {
+			console.error(error);
+			return null;
+		}
+	}
+
+	try {
+		const decoded = jwt.verify(token, signingKey);
+		console.log(decoded);
+		return decoded;
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+};
+
 const getAccessTokenFromHeaders = (headers) => {
 	const rawAuthorization = headers.authorization;
 	if (!rawAuthorization) {
@@ -9,8 +43,10 @@ const getAccessTokenFromHeaders = (headers) => {
 	}
 
 	const accessToken = authorizationParts[1];
+	return accessToken;
 };
 
 module.exports = {
 	getAccessTokenFromHeaders,
+	validateAccessToken,
 };
