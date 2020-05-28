45eb4e134c81d7b0139e5db7e0798c45c0352f12
Brian Clapner <brian.clapner@coxautoinc.com>
2020-05-28 11:38:56 -0400

    added exclaimation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { Accent, StyledTitle } from '../styled/Random';
 export default function Home() {
 	return (
 		<div>
-			<StyledTitle>Ready to type?</StyledTitle>
+			<StyledTitle>Ready to type?!</StyledTitle>
 			<CTA to="/game">
 				Click or type <Accent>'s'</Accent> to start playing!
 			</CTA>
