aeede5654a8cb8b69b8e320758190dd98f74aa7d
Brian Clapner <brian.clapner@coxautoinc.com>
2020-05-29 08:19:12 -0400

    theme persist in localStorage

--- src/hooks/UseTheme.js
+++ src/hooks/UseTheme.js
@@ -2,14 +2,13 @@ import { useEffect, useState } from 'react';
 
 export default () => {
 	const initialTheme = localStorage.getItem('theme');
-	const [theme, setTheme] = useState(initialTheme ? initialTheme : 'dark');
+	const [theme, setTheme] = useState(initialTheme || 'dark');
 
 	useEffect(() => {
 		localStorage.setItem('theme', theme);
 	}, [theme]);
 
 	const toggleTheme = () => {
-		console.log('toggle Theme');
 		setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
 	};
 
